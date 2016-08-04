fdc2c3141ea2d11e8f92b8e9cd4ea20fb12cffd8
wangli <wanglii@foxmail.com>
2016-08-04 10:12:10 +0800

    修复bug

--- main.cpp
+++ main.cpp
@@ -1,147 +1,148 @@
-//
-
-#include <iostream>
 #include <fstream>
+#include <string>
 #include <stdio.h>
 #include <io.h>
 #include <algorithm>
 #include <vector> 
+#include <iostream>
 using namespace std;
 
 
 void getFiles(string path, vector<string>& files)
 {
-	//文件句柄
-	long   hFile = 0;
-	//文件信息
-	struct _finddata_t fileinfo;
-	string p;
-	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
-	{
-		do
-		{
-			//如果是目录,迭代之
-			//如果不是,加入列表
-			if ((fileinfo.attrib &  _A_SUBDIR))
-			{
-				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
-					getFiles(p.assign(path).append("\\").append(fileinfo.name), files);
-			}
-			else
-			{
-				string str = fileinfo.name;
-				str = str.substr(str.rfind("."), str.length());
-				//transform(str.begin(), str.end(), str.begin(), tolower);
-				if (str == ".ppt" || str == ".pptx")
-				{
-					files.push_back(p.assign(path).append("\\").append(fileinfo.name));
-				}
-			}
-		} while (_findnext(hFile, &fileinfo) == 0);
-		_findclose(hFile);
-	}
+    //文件句柄
+    long   hFile = 0;
+    //文件信息
+    struct _finddata_t fileinfo;
+    string p;
+    if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
+    {
+        do
+        {
+            //如果是目录,迭代之
+            //如果不是,加入列表
+            if ((fileinfo.attrib &  _A_SUBDIR))
+            {
+                if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+                    getFiles(p.assign(path).append("\\").append(fileinfo.name), files);
+            }
+            else
+            {
+                string str = fileinfo.name;
+                str = str.substr(str.rfind("."), str.length());
+                //transform(str.begin(), str.end(), str.begin(), tolower);
+                if (str == ".ppt" || str == ".pptx")
+                {
+                    files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+                }
+            }
+        } while (_findnext(hFile, &fileinfo) == 0);
+        _findclose(hFile);
+    }
 }
 
 string&   replace_all(string&   str, const   string&   old_value, const   string&   new_value)
 {
-	while (true) {
-		string::size_type   pos(0);
-		if ((pos = str.find(old_value)) != string::npos)
-			str.replace(pos, old_value.length(), new_value);
-		else   break;
-	}
-	return   str;
+    while (true) {
+        string::size_type   pos(0);
+        if ((pos = str.find(old_value)) != string::npos)
+            str.replace(pos, old_value.length(), new_value);
+        else   break;
+    }
+    return   str;
 }
 
 int main(int argc, char** argv)
 {
-	cout << "ppt path" << endl;
+    cout << "ppt path" << endl;
 
-	string file;
-	cin >> file;
+    string file;
+    std::cin >> file;
 
-	vector<string> vec;
-	vec.reserve(100);
-	getFiles(file, vec);
+    vector<string> vec;
+    vec.reserve(100);
+    getFiles(file, vec);
 
-	cout << "start decode ...";
+    cout << "start decode ...";
 
-	for (size_t i = 0; i < vec.size(); ++i)
-	{
-		char sz[10] = { 0 };
+    for (size_t i = 0; i < vec.size(); ++i)
+    {
+        char sz[10] = { 0 };
 
-		string genPath = string("D:\\") + itoa(i, sz, 10);
+        _itoa_s(i, sz, 10);
 
-        cout<<"第" + i <<"个文件,文件路径:" + vec[i]<<endl;
-		 
-		string cmd = string("7z x ") + "\"" + vec[i] + "\" -o" + genPath;
+        string genPath = string("D:\\") + sz;
 
-		cout<<"开始转换..."<<endl;
-		 
-		system(cmd.c_str());
+        cout << "第" + i << "个文件,文件路径:" + vec[i] << endl;
 
-		string filename = vec[i].substr(vec[i].rfind("\\") + 1, vec[i].rfind(".") + 1);
+        string cmd = string("7z x ") + "\"" + vec[i] + "\" -o" + genPath;
 
-		filename = filename.substr(0, filename.rfind("."));
+        cout << "开始转换..." << endl;
 
-		string vvv = genPath + string("\\ppt\\presentation.xml");
-		ifstream in(vvv.c_str());
+        system(cmd.c_str());
 
-		istreambuf_iterator<char> beg(in), end;
+        string filename = vec[i].substr(vec[i].rfind("\\") + 1, vec[i].rfind(".") + 1);
 
-		string str(beg, end);
+        filename = filename.substr(0, filename.rfind("."));
 
-		size_t left = str.find("<p:modifyVerifier");
+        string vvv = genPath + string("\\ppt\\presentation.xml");
+        ifstream in(vvv.c_str());
 
-		string strLeft = str.substr(0, left);
+        istreambuf_iterator<char> beg(in), end;
 
-		string temp = str.substr(left, str.length());
+        string str(beg, end);
 
-		string strRight = temp.substr(temp.find("/>") + 2, temp.length());
+        size_t left = str.find("<p:modifyVerifier");
 
-		string total = strLeft + strRight;
-		
-		
-		vvv = genPath + string("\\ppt\\presentation.xml"); 
-		ofstream out(vvv.c_str());
+        string strLeft = str.substr(0, left);
 
-		out.write(total.c_str(), total.size());
+        string temp = str.substr(left, str.length());
 
-		out.flush();
+        string strRight = temp.substr(temp.find("/>") + 2, temp.length());
 
-		out.close();
+        string total = strLeft + strRight;
 
-		in.close();
-		
-		cout<<"解密完成!!!"<<endl;
 
-		replace_all(filename, "?", " ");
+        vvv = genPath + string("\\ppt\\presentation.xml");
+        ofstream out(vvv.c_str());
 
-		cmd = "7z a \"d:\\temp\\" + filename + ".zip\" " + genPath + "\\*";
+        out.write(total.c_str(), total.size());
 
-		system(cmd.c_str());
-		
-		cout<<"压缩完成!!!"<<endl;
+        out.flush();
 
-		string old = string("D:\\temp\\") + filename + string(".zip");
+        out.close();
 
-		string newfile = string("D:\\temp\\") + filename + string(".pptx");
+        in.close();
 
-		rename(old.c_str(), newfile.c_str());
+        cout << "解密完成!!!" << endl;
 
-		remove(old.c_str());
+        replace_all(filename, "?", " ");
 
-		string tmp = string("rd /s/q ") + genPath;
+        cmd = "7z a \"d:\\temp\\" + filename + ".zip\" " + genPath + "\\*";
 
-		system(tmp.c_str());
-		
-		cout<<"删除缓存"<<endl;
-	}
+        system(cmd.c_str());
 
-	cout<<"全部转换完成啦 ^-^ "<<endl;
-	
-	system("pause");
+        cout << "压缩完成!!!" << endl;
 
-	return 0;
+        string old = string("D:\\temp\\") + filename + string(".zip");
+
+        string newfile = string("D:\\temp\\") + filename + string(".pptx");
+
+        rename(old.c_str(), newfile.c_str());
+
+        remove(old.c_str());
+
+        string tmp = string("rd /s/q ") + genPath;
+
+        system(tmp.c_str());
+
+        cout << "删除缓存" << endl;
+    }
+
+    cout << "全部转换完成啦 ^-^ " << endl;
+
+    system("pause");
+
+    return 0;
 }
 
